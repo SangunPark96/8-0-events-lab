bfcb0ddc3826c62dc0a3a28e9573ef84f2e4fac6
SangUn Park <Sangunpark1@gmail.com>
2022-12-15 12:46:26 -0500

    events lab

--- main.js
+++ main.js
@@ -7,3 +7,50 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+
+//Sets currentColor Box
+let currentColor = document.querySelector("#current-color");
+
+ 
+
+document.querySelectorAll(".color").forEach((color) => {
+   color.addEventListener("click", () => {
+     currentColor.style.background = color.style.background;
+   });
+ });
+
+ 
+ 
+ document.querySelectorAll(".cell").forEach((cell) => {
+   cell.addEventListener("click", () => {
+     cell.style.background = currentColor.style.background;
+   });
+  });   
+  
+  
+  
+  
+  const resetBtn = document.querySelector(".resetter");
+
+  resetBtn.addEventListener("click", () => {
+  
+    const colorPaletteCells = document.querySelectorAll(".cell");
+  
+    colorPaletteCells.forEach( cell => cell.style.background = "white");
+    
+    currentColor.style.background = "white";
+    
+  });
+
+  // const paintAll = document.querySelector(".paintAll");
+
+  // paintAll.addEventListener("click", () => {
+
+  //   colorPaletteCells.forEach( cell => {
+      
+  //     cell.style.background = currentColor.style.background 
+    
+  //   });
+  // });
+
